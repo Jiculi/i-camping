63591b17eae8c6e142f166277aa549ecf25931ee
yann@linuxconsole.org <yann@linuxconsole.org@449271e4-e389-f7d1-d194-20a11754599c>
2012-03-14 20:11:51 +0000

    make picture

--- trunk/src/fr/icamping/car/addArea.java
+++ trunk/src/fr/icamping/car/addArea.java
@@ -1,10 +1,16 @@
 package fr.icamping.car;
 
-
 import java.io.File;
+import java.util.List;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,32 +20,40 @@ import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class addArea extends Activity {
     /** Called when the activity is first created. */
-	
+	private Uri mImageCaptureUri;
 	Spinner updateFreqSpinner;
-	static  int TAKE_PICTURE = 1;
-	private Uri outputFileUri;
-		
-	  @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {  
-		  Log.d("Yann", "Dans onActivityResult ");
-		  /*
-	        if (requestCode == TAKE_PICTURE) {  
-	        	Uri imageUri = null;
-	        	if(data != null)
-	        	{
-	        		if(data.hasExtra("data"))
-	        		{
-	        		    Bitmap thumbnail = (Bitmap) data.getExtras().get("data");  
-	        		    ImageView image = (ImageView) findViewById(R.id.photoResultView);  
-	        		    image.setImageBitmap(thumbnail);  
-	        		}
-	        		}
-	        	}
-	        	*/
-	        }  
-	  
+	private static final int PICK_FROM_CAMERA = 1;
+	 ImageView imageiewImageCaptured;
+	@Override
+	 protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	  // TODO Auto-generated method stub
+	  super.onActivityResult(requestCode, resultCode, data);
+	   
+	  if (resultCode == RESULT_OK)
+	  {
+		    imageiewImageCaptured = (ImageView)findViewById(R.id.photoResultView);
+		      
+	   Bundle extras = data.getExtras();
+	   Bitmap bmp = (Bitmap) extras.get("data");
+	   imageiewImageCaptured.setImageBitmap(bmp);
+	  }
+	   
+	 }
+	/*
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	
+		    	mImageCaptureUri = data.getData();
+		    	Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+     		    ImageView image = (ImageView) findViewById(R.id.photoResultView);
+     		    image.setImageBitmap(thumbnail);
+	    }
+	   */
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,16 +62,37 @@ public class addArea extends Activity {
         populateSpinners();
         Log.d("Yann", "Lancement make_picture");
         make_picture();
+        
      
     }
-    
-    private void make_picture() { 
-    	   Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-    	  	  File file = new File(Environment.getExternalStorageDirectory(),"area.jpg");
-    	  	  outputFileUri = Uri.fromFile(file);
-    	  	  takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-    	  	  startActivityForResult(takePictureIntent, TAKE_PICTURE);
-    }
+       
+    private void make_picture() {
+    	/*
+    	//define the file-name to save photo taken by Camera activity
+    	String fileName = "new-photo-name.jpg";
+    	//create parameters for Intent with filename
+    	ContentValues values = new ContentValues();
+    	values.put(MediaStore.Images.Media.TITLE, fileName);
+    	values.put(MediaStore.Images.Media.DESCRIPTION,"Image capture by camera");
+    	//imageUri is the current activity attribute, define and save it for later usage (also in onSaveInstanceState)
+    	
+    	mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),
+				   "tmp_avatar_" + String.valueOf(System.currentTimeMillis()) + ".jpg"));
+
+    	Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    	intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+    	intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+    	try {
+			intent.putExtra("return-data", true);
+			startActivityForResult(intent, PICK_FROM_CAMERA);
+		} catch (ActivityNotFoundException e) {
+			e.printStackTrace();
+		}
+       */
+    	  Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+    	   startActivityForResult(intent, 0);
+    	   
+    	}
     
     private void populateSpinners() { 
     	ArrayAdapter<CharSequence> fAdapter;
@@ -67,4 +102,5 @@ public class addArea extends Activity {
 	    updateFreqSpinner.setAdapter(fAdapter);
     
     }
+
 }
