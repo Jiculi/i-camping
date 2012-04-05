542dffb1186be71a430e96f31a94b8a3dd6c33a6
yann@linuxconsole.org <yann@linuxconsole.org@449271e4-e389-f7d1-d194-20a11754599c>
2012-04-05 13:56:52 +0000

    fix image preview, clean buttons

--- trunk/src/fr/icamping/car/campingCarActivity.java
+++ trunk/src/fr/icamping/car/campingCarActivity.java
@@ -1,28 +1,88 @@
 package fr.icamping.car;
 
-import java.io.File;
+
 
 import android.app.Activity;
+import android.app.AlertDialog;
+
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
+
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+
 
 public class campingCarActivity extends Activity {
     /** Called when the activity is first created. */
 
+	
+
+
+	private void exitIcamping()
+	{
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    	builder.setMessage(getString(R.string.sureExit))
+    	       .setCancelable(false)
+    	       .setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+    	           public void onClick(DialogInterface dialog, int id) {
+    	        	   campingCarActivity.this.finish();
+    	           }
+    	       })
+    	       .setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
+    	           public void onClick(DialogInterface dialog, int id) {
+    	                dialog.cancel();
+    	           }
+    	       });
+    	AlertDialog alert = builder.create();
+    	builder.show();
+	}
+	
+	private void aboutIcamping()
+	{
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    	builder.setMessage(getString(R.string.about_text));
+    	builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {  
+  	      public void onClick(DialogInterface dialog, int which) {  
+  	        return;  
+  	    } });   
+    	AlertDialog alert = builder.create();
+    	
+    	builder.show();
+	}
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        Button buttonExit = (Button) findViewById(R.id.ButtonExit);
+        buttonExit.setOnClickListener(new android.view.View.OnClickListener() {
+            public void onClick(View view) {
+        
+            	exitIcamping();
+            	
+            	
+            }
+        }
+        );
+        
+        Button buttonAbout = (Button) findViewById(R.id.ButtonAbout);
+        buttonAbout.setOnClickListener(new android.view.View.OnClickListener() {
+            public void onClick(View view) {
+        
+            	aboutIcamping();
+            	
+            	
+            }
+        }
+        );
+
+
+      
+
     // Intent intent = new Intent(this, addArea.class);
      //startActivity(intent);
            
